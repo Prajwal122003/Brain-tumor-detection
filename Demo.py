bc3a6d75d91d53a26ff3fae22dd5b210c97c3dab
prajwal_chandra <83130088+Prajwal122003@users.noreply.github.com>
2024-02-28 21:35:55 +0530

    Added New FIles

---
+++ Demo.py
@@ -0,0 +1,9 @@
+#import os
+
+#os.environ['TCL_LIBRARY']="C:/Users/bandi/AppData/Local/Programs/Python/Python310/tcl/tcl8.6"
+#os.environ['TK_LIBRARY']="C:/Users/bandi/AppData/Local/Programs/Python/Python310/tcl/tk8.6"
+
+#import os
+
+#os.environ['TCL_LIBRARY']="C:/Users/bandi/anaconda3/Library/lib/tcl8.6"
+#os.environ['TCL_LIBRARY']="C:/Users/bandi/anaconda3/Library/lib/tcl8.6"
